af0461a3bbb40897921a1ee30b6a8b560820820a
Omar Ortega <omarvalentin@gmail.com>
2020-09-03 18:32:28 -0500

    PPrimer commit peración get para usuarios

---
+++ db/config.js
@@ -0,0 +1,17 @@
+
+require("dotenv").config()
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:",
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
+}
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../db/models');
+//const db = require('../db/config');
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  var filter = req.body.filter || {}
+  return db.User.findAll(filter).then((users) => res.json(users))
+})
+
+
+module.exports = router;
