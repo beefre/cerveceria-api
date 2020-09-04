b83f151724c2f1f184425d2c9321399feaaebf44
Omar Ortega <omarvalentin@gmail.com>
2020-09-04 13:30:27 -0500

    wip

--- routes/users.js
+++ routes/users.js
@@ -4,27 +4,34 @@ const db = require('../db/models');
 //const db = require('../db/config');
 
 /* GET users listing. */
-// router.get("/", function (req, res, next) {
-//   var filter = req.body.filter || {}
-//   return db.User.findAll(filter).then((users) => res.json(users))
+router.get("/", function (req, res, next) {
+   var filter = req.body.filter || {}
+  // return db.User.findAll(filter).then((users) => res.json(users))
   
-//   //return res.json({success:"exito"})
-// })
+  //return res.json({success:"exito"})
 
-router.get("/", function (req, res) {
   try {
+    return db.User.findAll(filter).then((users) => res.json(users))
+  } catch (error) {
+    console.log(error)
     
-    db.User.find({},(err,users)=>{ 
-              
-        res.send({users})
+  }
+})
 
-    });
+// router.get("/", function (req, res) {
+//   try {
+    
+//     db.User.findAll({},(err,users)=>{ 
+              
+//         res.send({users})
+
+//     });
     
   
-  } catch (error) {
-    console.log("Ligas not found: ",error);
-  }
-});
+//   } catch (error) {
+//     console.log("Ligas not found: ",error);
+//   }
+// });
 
 
 module.exports = router;
