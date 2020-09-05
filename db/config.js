6b109aa7ce56cc9bf9bf49fffc6ba75901b3a1d8
Omar Ortega <omarvalentin@gmail.com>
2020-09-05 12:34:50 -0500

    se creo diferenciación variable de entorno para conexión a base de datos

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "DATABASE_DEV",
     dialect: "postgres",
   },
   test: {
